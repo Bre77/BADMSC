8548d53a293caaa493faa24ab50ddbd09c0c64ce
Brett Adams <brett.whynot@gmail.com>
2023-03-12 18:38:16 +1000

    Untested Private App code

--- packages/badmsc/src/main/resources/splunk/bin/privateapp.py
+++ packages/badmsc/src/main/resources/splunk/bin/privateapp.py
@@ -6,6 +6,7 @@ import tarfile
 from io import BytesIO
 import os
 from pathlib import Path
+import time
 
 SPLUNK_HOME = os.environ["SPLUNK_HOME"]
 
@@ -29,31 +30,85 @@ class request(PersistentServerConnectionApplication):
             }
 
         try:
-            options = json.loads(args["payload"])
+            details = json.loads(args["payload"])
         except Exception as e:
             self.logger.info(f"Invalid payload. {e}")
             return {"payload": "Invalid JSON payload", "status": 400}
 
-        app_dir = Path(os.path.join(SPLUNK_HOME, "etc", "apps", options["app"]))
-        tar_bytes_io = BytesIO()
-        # Create the tar file
-        with tarfile.open(fileobj=tar_bytes_io, mode="w:gz") as tar:
-            # Add all files and directories in the app directory to the tar file
-            for file_path in app_dir.glob("**"):
-                # Exclude the local directory
-                self.logger.info(file_path)
-                if file_path.relative_to(app_dir).parts[0] != "local":
+        try:
+            app_dir = Path(os.path.join(SPLUNK_HOME, "etc", "apps", details["app"]))
+            tar_bytes_io = BytesIO()
+            # Create the tar file
+            with tarfile.open(fileobj=tar_bytes_io, mode="w:gz") as tar:
+                # Add all files and directories in the app directory to the tar file
+                for file_path in app_dir.glob("**"):
+                    # Exclude the local directory
+                    self.logger.info(file_path)
+                    if file_path.relative_to(app_dir).parts[0] != "local":
 
-                    tar.add(str(file_path), arcname=file_path.relative_to(app_dir))
+                        tar.add(str(file_path), arcname=file_path.relative_to(app_dir))
 
-        # Get the bytes of the tar file
-        package = tar_bytes_io.getvalue()
+            # Get the bytes of the tar file
+            package = tar_bytes_io.getvalue()
+        except Exception as e:
+            return {"payload": str(e), "status": 500}
 
-        # Write tar_bytes_io to disk
+        # Write tar_bytes_io to disk for debugging
         with open(
-            os.path.join(SPLUNK_HOME, "dev", "BADMSC", options["app"] + ".tar.gz"), "wb"
+            os.path.join(SPLUNK_HOME, "dev", "BADMSC", details["app"] + ".tar.gz"), "wb"
         ) as f:
             f.write(package)
 
-        return {"payload": str(app_dir), "status": 200}
-        raise Exception("end")
+        # Upload to AppInspect
+        upload = requests.post(
+            "https://appinspect.splunk.com/v1/app/validate",
+            files={
+                "app_package": package,
+                "included_tags": (None, "private_app"),
+            },
+            headers={"Authorization": f"Bearer {details['token']}"},
+        )
+        rid = upload.json()["request_id"]
+
+        ready = False
+        while not ready:
+            # Check if its done every 2 seconds
+            time.sleep(2)
+            check = requests.get(
+                f"https://appinspect.splunk.com/v1/app/validate/status/{rid}",
+                headers={"Authorization": f"Bearer {details['token']}"},
+            )
+            status = check.json()["status"]
+            ready = status == "SUCCESS"
+
+        result = requests.get(
+            f"https://appinspect.splunk.com/v1/app/report/{rid}",
+            headers={"Authorization": f"Bearer {details['token']}"},
+        )
+
+        # Write result to disk for debugging
+        with open(
+            os.path.join(SPLUNK_HOME, "dev", "BADMSC", details["app"] + ".json"), "wb"
+        ) as f:
+            f.write(result.text)
+
+        data = result.json()
+
+        if not (
+            data["summary"]["failure"] == 0
+            and data["summary"]["error"] == 0
+            and data["summary"]["manual_check"] == 0
+        ):
+            return {"payload": data, "status": 200}
+
+        # Upload via ACS
+        upload = requests.post(
+            f"https://{details['dstacs']}/adminconfig/v2/apps/victoria",
+            data=package,
+            headers={
+                "X-Splunk-Authorization": details["token"],
+                "Authorization": f"Bearer {details['dsttoken']}",
+                "ACS-Legal-Ack": "Y",
+            },
+        )
+        return {"payload": upload.json(), "status": 201}
