3074c826ad13dc4d35671e3fd88dbed1a4f8094e
Brett Adams <brett.whynot@gmail.com>
2023-05-10 00:25:22 +0000

    Add data plan

--- src/main/webapp/shared/hooks.jsx
+++ src/main/webapp/shared/hooks.jsx
@@ -1,28 +1,28 @@
-import React from 'react';
-import { useQuery, useQueries } from '@tanstack/react-query';
-import { splunkdPath, username } from '@splunk/splunk-utils/config';
-import { defaultFetchInit } from '@splunk/splunk-utils/fetch';
-import { request } from './fetch';
-import { CONF_FILES } from './const';
+import { splunkdPath, username } from "@splunk/splunk-utils/config";
+import { defaultFetchInit } from "@splunk/splunk-utils/fetch";
+import { useQueries, useQuery } from "@tanstack/react-query";
+import React, { useReducer } from "react";
+import { CONF_FILES } from "./const";
+import { request } from "./fetch";
+import { localLoad } from "./helpers";
 
 export const LOCAL_URL = `${splunkdPath}/servicesNS/${username}/badmsc`;
 
-export const handle = (res) => 
-    res.ok ? res.json() : res.text().then(text=>{
-        console.error(text);
-        return Promise.reject(text)
-    })
+export const handle = (res) =>
+    res.ok
+        ? res.json()
+        : res.text().then((text) => {
+              console.error(text);
+              return Promise.reject(text);
+          });
 
 const entry = (data) => data.entry;
 
 export const useConfig = () =>
     useQuery({
-        queryKey: ['config'],
+        queryKey: ["config"],
         queryFn: () =>
-            fetch(
-                `${LOCAL_URL}/storage/passwords/badmsc%3Aauth%3A?output_mode=json&count=1`,
-                defaultFetchInit
-            ).then((res) => {
+            fetch(`${LOCAL_URL}/storage/passwords/badmsc%3Aauth%3A?output_mode=json&count=1`, defaultFetchInit).then((res) => {
                 if (res.status === 404) {
                     return false;
                 }
@@ -31,8 +31,8 @@ export const useConfig = () =>
                         .json()
                         .then((data) => JSON.parse(data.entry[0].content.clear_password))
                         .then((config) => {
-                            config.src.key = 'src';
-                            config.dst.key = 'dst';
+                            config.src.key = "src";
+                            config.dst.key = "dst";
                             return config;
                         })
                         .catch(() => Promise.reject());
@@ -40,7 +40,7 @@ export const useConfig = () =>
                 return Promise.reject();
             }),
 
-        notifyOnChangeProps: ['data'],
+        notifyOnChangeProps: ["data"],
     });
 
 export const useApi = (target, path, postprocess = entry, staleTime = Infinity) =>
@@ -49,8 +49,8 @@ export const useApi = (target, path, postprocess = entry, staleTime = Infinity) 
         queryFn: () =>
             request({
                 url: `${target.api}/${path}`,
-                method: 'GET',
-                params: { output_mode: 'json', count: -1 },
+                method: "GET",
+                params: { output_mode: "json", count: -1 },
                 headers: {
                     Authorization: `Bearer ${target.token}`,
                 },
@@ -63,17 +63,15 @@ export const useApi = (target, path, postprocess = entry, staleTime = Infinity) 
 
 export const useApps = (target) =>
     // useApps is called in multiple steps, so is defined once for consistency
-    useApi(target, 'services/apps/local', (data) =>
-        Object.fromEntries(data.entry.map((app) => [app.name, app]))
-    );
+    useApi(target, "services/apps/local", (data) => Object.fromEntries(data.entry.map((app) => [app.name, app])));
 
 export const useAcs = (target, endpoint) =>
     useQuery({
-        queryKey: ['acs', endpoint],
+        queryKey: ["acs", endpoint],
         queryFn: () =>
             request({
                 url: `${target.acs}/adminconfig/v2/${endpoint}`,
-                method: 'GET',
+                method: "GET",
                 params: { count: 0 },
                 headers: {
                     Authorization: `Bearer ${target.token}`,
@@ -97,12 +95,12 @@ export const processConfs = (data) =>
 export const useConfs = (target, files = CONF_FILES) =>
     useQueries({
         queries: files.map((file) => ({
-            queryKey: [target.key, 'config', file],
+            queryKey: [target.key, "config", file],
             queryFn: () =>
                 request({
                     url: `${target.api}/servicesNS/nobody/-/configs/conf-${file}`,
-                    method: 'GET',
-                    params: { output_mode: 'json', count: -1 },
+                    method: "GET",
+                    params: { output_mode: "json", count: -1 },
                     headers: {
                         Authorization: `Bearer ${target.token}`,
                     },
@@ -115,12 +113,12 @@ export const useConfs = (target, files = CONF_FILES) =>
 export const useDefaults = (target, files = CONF_FILES) =>
     useQueries({
         queries: files.map((file) => ({
-            queryKey: [target.key, 'default', file],
+            queryKey: [target.key, "default", file],
             queryFn: () =>
                 request({
                     url: `${target.api}/services/properties/${file}/default`,
-                    method: 'GET',
-                    params: { output_mode: 'json', count: -1 },
+                    method: "GET",
+                    params: { output_mode: "json", count: -1 },
                     headers: {
                         Authorization: `Bearer ${target.token}`,
                     },
@@ -135,3 +133,10 @@ export const useDefaults = (target, files = CONF_FILES) =>
                     .catch(() => ({})),
         })),
     });
+
+export const useLocal = (key, fallback) => {
+    return useReducer((prev, value) => {
+        value === null ? window.localStorage.removeItem(key) : window.localStorage.setItem(key, JSON.stringify(value));
+        return value;
+    }, localLoad(key, fallback));
+};
--- src/main/webapp/steps/data.jsx
+++ src/main/webapp/steps/data.jsx
@@ -1,18 +1,67 @@
-import React from 'react';
+import ControlGroup from "@splunk/react-ui/ControlGroup";
+import Date from "@splunk/react-ui/Date";
+import Heading from "@splunk/react-ui/Heading";
+import Number from "@splunk/react-ui/Number";
+import P from "@splunk/react-ui/Paragraph";
+import Table from "@splunk/react-ui/Table";
+import Text from "@splunk/react-ui/Text";
+import { useMutation } from "@tanstack/react-query";
+import moment from "moment";
+import React, { useState } from "react";
+import MutateButton from "../components/mutateButton";
+import { request } from "../shared/fetch";
+import { wrapSetValue } from "../shared/helpers";
+import { handle, useLocal } from "../shared/hooks";
 
-// Splunk UI
-import Heading from '@splunk/react-ui/Heading';
-import P from '@splunk/react-ui/Paragraph';
+export default ({ step, config }) => {
+    const [search, setSearch] = useState("index = *");
+    const handleSearch = wrapSetValue(setSearch);
+    const [earliest, setEarliest] = useState(moment().add(-90, "day").format("YYYY-MM-DD"));
+    const handleEarliest = wrapSetValue(setEarliest);
+    const [latest, setLatest] = useState(moment().format("YYYY-MM-DD"));
+    const handleLatest = wrapSetValue(setLatest);
+    const [plan, setPlan] = useLocal("badmsc_migration_plan", false);
 
-export default ({ step }) => {
+    const summary = useMutation(() =>
+        request({
+            url: `${config.src.api}/services/search/jobs`,
+            method: "POST",
+            params: { output_mode: "json", count: -1 },
+            data: {
+                search: `| tstats count where ${search} by index _time span=1d | xyseries index _time count`,
+                earliest_time: moment(earliest).unix(),
+                latest_time: moment(latest).add(1, "day").unix(),
+                output_mode: "json",
+                exec_mode: "oneshot",
+                count: -1,
+            },
+            headers: {
+                Authorization: `Bearer ${config.src.token}`,
+            },
+        })
+            .then(handle)
+            .then((data) => setPlan(data.results))
+    );
     return (
         <div>
             <P>
-                User (private) knowledge objects can be transferred as long as the user exists in
-                Splunk Cloud. If you are using SSO then this requires them to have logged in once.
+                User (private) knowledge objects can be transferred as long as the user exists in Splunk Cloud. If you are using SSO then this requires them to
+                have logged in once.
             </P>
             <Heading level={2}>Step {step} Option 1 - Dual Forwarding</Heading>
             <Heading level={2}>Step {step} Option 2 - _raw Event Copy</Heading>
+            <ControlGroup label="Earliest">
+                <Date highlightToday value={earliest} onChange={handleEarliest} />
+            </ControlGroup>
+            <ControlGroup label="Latest">
+                <Date highlightToday value={latest} onChange={handleLatest} />
+            </ControlGroup>
+            <ControlGroup label="Search">
+                <Text value={search} onChange={handleSearch} /> Hours
+            </ControlGroup>
+            <ControlGroup label=" ">
+                <MutateButton mutation={summary} label="Create New Migration Plan" />
+            </ControlGroup>
         </div>
     );
 };
