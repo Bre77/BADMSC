3e0626a3be2d9d26041fd5d5695d2a364cb6bc04
Brett Adams <brett.whynot@gmail.com>
2023-05-11 15:11:44 +1000

    Work on HEC

--- src/main/webapp/steps/hec.jsx
+++ src/main/webapp/steps/hec.jsx
@@ -3,108 +3,71 @@ import Heading from '@splunk/react-ui/Heading';
 import P from '@splunk/react-ui/Paragraph';
 import Table from '@splunk/react-ui/Table';
 import Typography from '@splunk/react-ui/Typography';
-import { normalizeBoolean } from '@splunk/splunk-utils/boolean';
 import React, { useMemo } from 'react';
-import { ATTR_BLACKLIST } from '../shared/const';
 import { useApi, useConfs, useDefaults } from '../shared/hooks';
 
+const processHecs = (data) =>
+    Object.fromEntries(
+        data.entry.map(({ name, content }) => [
+            name.replace('http://', ''),
+            Object.fromEntries(
+                Object.entries(content).filter(
+                    ([key]) => !key.startsWith('_') && !key.startsWith('eai:')
+                )
+            ),
+        ])
+    );
+
 export default ({ step, config }) => {
-    const src = useApi(config.src, '/servicesNS/nobody/-/data/inputs/http');
-    const dst = useApi(config.dst, '/servicesNS/nobody/-/data/inputs/http');
+    const src = useApi(config.src, '/servicesNS/nobody/-/data/inputs/http', processHecs);
+    const dst = useApi(config.dst, '/servicesNS/nobody/-/data/inputs/http', processHecs);
+
+    const hec = useMemo(() => {
+        if (!src.data || !dst.data) return [];
+        const output = [];
+        Object.entries(src.data).forEach(([name, content]) => {
+            if (!dst.data[name]) {
+                output.push({ name, content });
+            }
+        });
+        return Object.entries(src.data);
+    }, [src.data, dst.data]);
 
     return (
         <div>
             <P>
                 HEC Inputs cannot be created from inputs.conf as they need to be pushed to the
-                indexers
+                indexers.
             </P>
-            <P>
-                If there will be no parsing in Splunk Cloud (using Heavy Forwarders) you can skip
-                this step.
-            </P>
-            <Heading level={2}>Step {step}.1 - Copy/Select Sourcetypes</Heading>
+            <Heading level={2}>Step {step}.1 - Migrate HEC Inputs</Heading>
             <Table stripeRows>
                 <Table.Head>
+                    <Table.HeadCell>Name</Table.HeadCell>
                     <Table.HeadCell>Local</Table.HeadCell>
                     <Table.HeadCell>Cloud</Table.HeadCell>
                     <Table.HeadCell>Copy</Table.HeadCell>
                 </Table.Head>
                 <Table.Body>
-                    {props.map(([stanza, content, present]) => (
-                        <Table.Row key={stanza}>
+                    {hec.map(([name, content]) => (
+                        <Table.Row key={name}>
+                            <Table.Cell>{name}</Table.Cell>
                             <Table.Cell>
-                                <Typography as="code" variant="monoBody">
-                                    [{stanza}]<br />
-                                    {content.map(([a, src, dst], i) => (
-                                        <span key={i}>
-                                            {src !== undefined && `${a} = ${src}`}
-                                            <br />
-                                        </span>
-                                    ))}
+                                <Typography as="p" variant="monoSmallBody">
+                                    {JSON.stringify(content)}
                                 </Typography>
                             </Table.Cell>
                             <Table.Cell>
-                                {present && (
-                                    <Typography as="code" variant="monoBody">
-                                        [{stanza}]<br />
-                                        {content.map(([a, src, dst], i) => (
-                                            <span key={i}>
-                                                {dst !== undefined && `${a} = ${dst}`}
-                                                <br />
-                                            </span>
-                                        ))}
-                                    </Typography>
-                                )}
+                                <Typography as="p" variant="monoSmallBody">
+                                    n/a
+                                </Typography>
                             </Table.Cell>
                             <Table.Cell>
-                                <Button>Copy</Button>
+                                <Button appearance="primary">Copy</Button>
                             </Table.Cell>
                         </Table.Row>
                     ))}
                 </Table.Body>
             </Table>
-            <Heading level={2}>Step {step}.2 - Select Transforms</Heading>
-            <Table stripeRows>
-                <Table.Head>
-                    <Table.HeadCell>Local</Table.HeadCell>
-                    <Table.HeadCell>Cloud</Table.HeadCell>
-                    <Table.HeadCell>Copy</Table.HeadCell>
-                </Table.Head>
-                <Table.Body>
-                    {transforms.map(([stanza, content, present]) => (
-                        <Table.Row key={stanza}>
-                            <Table.Cell>
-                                <Typography as="code" variant="monoBody">
-                                    [{stanza}]<br />
-                                    {content.map(([a, src, dst], i) => (
-                                        <span key={i}>
-                                            {src !== undefined && `${a} = ${src}`}
-                                            <br />
-                                        </span>
-                                    ))}
-                                </Typography>
-                            </Table.Cell>
-                            <Table.Cell>
-                                {present && (
-                                    <Typography as="code" variant="monoBody">
-                                        [{stanza}]<br />
-                                        {content.map(([a, src, dst], i) => (
-                                            <span key={i}>
-                                                {dst !== undefined && `${a} = ${dst}`}
-                                                <br />
-                                            </span>
-                                        ))}
-                                    </Typography>
-                                )}
-                            </Table.Cell>
-                            <Table.Cell>
-                                <Button>Copy</Button>
-                            </Table.Cell>
-                        </Table.Row>
-                    ))}
-                </Table.Body>
-            </Table>
-            <Heading level={2}>Step {step}.3 - Upload Private App</Heading>
         </div>
     );
 };
