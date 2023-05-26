e449524c9c6a77bba4506a946c92cd91827553d8
Brett Adams <brett.whynot@gmail.com>
2023-05-26 11:08:11 +1000

    Cleanup

--- src/main/webapp/steps/start.jsx
+++ src/main/webapp/steps/start.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 // Splunk UI
 import Heading from '@splunk/react-ui/Heading';
-import P from '@splunk/react-ui/Paragraph';
+import Link from '@splunk/react-ui/Link';
 import List from '@splunk/react-ui/List';
 import Message from '@splunk/react-ui/Message';
-import Link from '@splunk/react-ui/Link';
+import P from '@splunk/react-ui/Paragraph';
 
 export default ({ step, config }) => {
     return (
@@ -25,16 +25,16 @@ export default ({ step, config }) => {
             <Heading level={2}>Requirements and Limitations</Heading>
             <List>
                 <List.Item>
-                    Splunk App for Lookup File Editing is required to migrate Lookups
+                    Splunk App for Lookup File Editing is required on the source and destination
+                    system to migrate Lookups
                 </List.Item>
                 <List.Item>
                     Authentication to a user in Splunk Cloud with the sc_admin role is required
                 </List.Item>
                 <List.Item>
-                    No config modifications in app default directories will be migrated (except for
-                    private apps)
+                    Config modifications in app default directories will be migrated into local
+                    (except for private apps)
                 </List.Item>
-                <List.Item>No config modifications in system/local will be migrated</List.Item>
             </List>
         </div>
     );
