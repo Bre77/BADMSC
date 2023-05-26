accc1b3506c800280a30c0905e833c872fb503aa
Brett Adams <brett.whynot@gmail.com>
2023-05-26 02:28:26 +0000

    Little rework

---
+++ make.sh
@@ -0,0 +1,10 @@
+mv stage temp
+node bin/build.js build
+mv stage badmsc
+mv temp stage
+chmod -R u=rwX,go= badmsc/*
+chmod -R u-x+X badmsc/*
+chmod -R u=rwx,go= badmsc/bin/*
+tar -cpzf dist/badmsc.spl --exclude=badmsc/.* --exclude=badmsc/local badmsc
+rm -rf badmsc
+
