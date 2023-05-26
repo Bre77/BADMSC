6f445e9de71f9d26f22451ba60ff77d63161f461
Brett Adams <brett.whynot@gmail.com>
2023-05-26 14:29:19 +1000

    fix make

--- make.sh
+++ make.sh
@@ -5,6 +5,7 @@ mv temp stage
 chmod -R u=rwX,go= badmsc/*
 chmod -R u-x+X badmsc/*
 chmod -R u=rwx,go= badmsc/bin/*
+rm dist/badmsc.spl
 tar -cpzf dist/badmsc.spl --exclude=badmsc/.* --exclude=badmsc/local badmsc
 rm -rf badmsc
 
