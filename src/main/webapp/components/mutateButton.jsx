468d1a2495b7656fbefbd9eb7bffe3d63fd5f923
Brett Adams <brett.whynot@gmail.com>
2023-06-12 01:29:26 +0000

    Tweak mutateButton

--- src/main/webapp/components/mutateButton.jsx
+++ src/main/webapp/components/mutateButton.jsx
@@ -4,7 +4,7 @@ import React from "react";
 export default ({ mutation, label, disabled }) => (
     <Button
         appearance={{ idle: "default", loading: "pill", success: "primary", error: "destructive" }[mutation.status]}
-        onClick={() => mutation.mutate()}
+        onClick={mutation.mutate}
         disabled={mutation.isLoading || disabled}
         label={{ idle: label, loading: "Running", success: "Success", error: "Failed" }[mutation.status]}
     />
