a8d225e6b64512559f49ba4c3f782350fa074ef6
Brett Adams <brett.whynot@gmail.com>
2023-06-19 04:12:54 +0000

    tweak packaging

--- make.sh
+++ make.sh
@@ -5,7 +5,7 @@ mv stage2 stage
 chmod -R u=rwX,go= badmsc/*
 chmod -R u-x+X badmsc/*
 chmod -R u=rwx,go= badmsc/bin/*
-rm dist/badmsc.spl
-tar -cpzf dist/badmsc.spl --exclude=badmsc/.* --exclude=badmsc/local badmsc
+rm $1
+tar -cpzf $1 --exclude=badmsc/.* --exclude=badmsc/local badmsc
 rm -rf badmsc
 
