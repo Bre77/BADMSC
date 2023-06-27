83ef345e8fac6746a8eacd4fe2678b0c15322059
Brett Adams <brett.whynot@gmail.com>
2023-06-27 01:44:02 +0000

    Tweak Make

--- make.sh
+++ make.sh
@@ -5,7 +5,7 @@ mv stage2 stage
 chmod -R u=rwX,go= badmsc/*
 chmod -R u-x+X badmsc/*
 chmod -R u=rwx,go= badmsc/bin/*
-rm $1
-tar -cpzf $1 --exclude=badmsc/.* --exclude=badmsc/local badmsc
+rm badmsc.spl
+tar -cpzf badmsc.spl --exclude=badmsc/.* --exclude=badmsc/local badmsc
 rm -rf badmsc
 
