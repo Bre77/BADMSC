85e918dd5ab6fe06fda82328a46def25aaff510b
Brett Adams <brett.whynot@gmail.com>
2024-03-14 05:12:20 +0000

    Add to attr blacklist

--- src/main/webapp/shared/const.jsx
+++ src/main/webapp/shared/const.jsx
@@ -1,4 +1,4 @@
 export const CONF_FILES = ["props", "transforms", "macros", "tags", "eventtypes", "savedsearches"]; //"inputs", "collections",
-export const ATTR_BLACKLIST = ["eai:acl", "eai:appName", "eai:userName", "maxDist", "termFrequencyWeightedDist", "next_scheduled_time"];
+export const ATTR_BLACKLIST = ["eai:acl", "eai:appName", "eai:userName", "maxDist", "termFrequencyWeightedDist", "next_scheduled_time", "triggered_alert_count", "embed.enabled"];
 export const MAP_BLACKLIST = ["eai:acl", "eai:appName", "eai:userName", "disabled"];
 export const APP_ID = "badmsc";
